da2577253d029e34b2a6e3fae2c51b09c7ece5d1
yongelee <albertkim91@gmail.com>
2019-06-19 18:51:31 -0400

    r

--- src/db-scripts/insert/boxscoreToDb.ts
+++ src/db-scripts/insert/boxscoreToDb.ts
@@ -20,7 +20,7 @@ const getMatchIdsFromTournament = async () => {
   // for each tournament match id
   let tournaments = await Tournament.find();
 
-  tournaments = tournaments.slice(5, 10);
+  tournaments = tournaments.slice(10, 15);
 
   for (var z = 0; z < tournaments.length; z++) {
     // @ts-ignore
---
+++ src/twitter/tests/main.ts
@@ -0,0 +1,23 @@
+import 'dotenv/config'
+import Twit from 'twit'
+
+var T = new Twit({
+  consumer_key: process.env.TWITTER_CONSUMER_KEY as string,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET as string,
+  access_token: process.env.TWITTER_ACCESS_TOKEN as string,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET as string,
+  timeout_ms: 60 * 1000,  // optional HTTP request timeout to apply to all requests.
+  strictSSL: true,     // optional - requires SSL certificates to be valid.
+})
+
+
+const doit = async () => {
+  console.log('twit')
+  T.post('statuses/update', { status: "hello world" }, (err, data, res) => {
+    console.log(data)
+    // console.log(res)
+
+  })
+}
+
+doit();
